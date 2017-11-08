dc45c137b23f449e10e98db08c90f50513160bc3
Kerby Shedden <kshedden@umich.edu>
2017-11-07 21:46:36 -0500

    update

--- USRealEstate/year_built.py
+++ USRealEstate/year_built.py
@@ -26,39 +26,147 @@ nb = nb.reset_index()
 nb = nb.loc[nb["YEAR BUILT"] >= 1960, :]
 
 nb.columns = ["fips", "year", "numbuilt"]
-nb.numbuilt *= 100
 nb["year"] = nb["year"].astype(np.int)
 nb["lognumbuilt"] = np.log(nb.numbuilt)
 
-
-def absr(x):
-    x = np.asarray(x).ravel()
-    x = x[np.isfinite(x)]
-    return np.median(np.abs(x))
-
-xt = nb.pivot("fips", "year", "lognumbuilt")
-mp_years, mp_fips = 0, 0
-for k in range(3):
-    print(absr(xt))
-    mp_fips_0 = xt.median(axis=1)
-    xt = xt.subtract(np.outer(mp_fips_0, np.ones(xt.shape[1]))) 
-    mp_fips += mp_fips_0
-    mp_years_0 = xt.median(axis=0)
-    print(absr(xt))
-    xt = xt.subtract(np.outer(np.ones(xt.shape[0]), mp_years_0))
-    mp_years += mp_years_0
-
-print(absr(xt))
-
-1/0
-
-
 nb = pd.merge(cp, nb, left_on=["fips", "year"], right_on=["fips", "year"])
 
-model = sm.GLM.from_formula("numbuilt ~ bs(year, 10) + logpop", data=nb, family=sm.families.Poisson())
+# Fit a Poisson model
+model = sm.GLM.from_formula("numbuilt ~ bs(year, 10) * logpop", data=nb, family=sm.families.Poisson())
 result = model.fit()
 
+# Check the mean/variance relationship for the Poisson model
+qt = pd.qcut(result.fittedvalues, 50)
+dy = pd.DataFrame({"fit": result.fittedvalues, "obs": model.endog})
+qa = dy.groupby(qt).agg({"fit": np.mean, "obs": np.var})
+
 for alpha in 0.1, 0.25, 0.5, 0.75, 1:
-    model = sm.GLM.from_formula("numbuilt ~ bs(year, 10) + logpop", data=nb, family=sm.families.NegativeBinomial(alpha=alpha))
+    model = sm.GLM.from_formula("numbuilt ~ bs(year, 10) * logpop", data=nb, family=sm.families.NegativeBinomial(alpha=alpha))
     result = model.fit()
-    print(result.aic)
+    print(alpha, result.aic)
+
+model1 = sm.GEE.from_formula("numbuilt ~ bs(year, 10) * logpop", data=nb, family=sm.families.NegativeBinomial(alpha=0.5),
+                            cov_struct=sm.cov_struct.Exchangeable(), groups="fips")
+result1 = model1.fit(maxiter=50)
+
+model2 = sm.GEE.from_formula("numbuilt ~ bs(year, 10) * logpop", data=nb, family=sm.families.NegativeBinomial(alpha=0.5),
+                             cov_struct=sm.cov_struct.Stationary(max_lag=20), time=nb.year-1960, groups="fips")
+result2 = model2.fit(maxiter=50)
+
+model3 = sm.GEE.from_formula("numbuilt ~ bs(year, 10) * logpop", data=nb, family=sm.families.NegativeBinomial(alpha=0.5),
+                             cov_struct=sm.cov_struct.Exchangeable(), time=nb.year-1960, groups="fips")
+result3 = model3.fit()
+
+
+from statsmodels.sandbox import predict_functional
+pred1, cb1, fvals1 = predict_functional.predict_functional(result3, "year", ci_method="simultaneous", values={"logpop": 10})
+pred2, cb2, fvals2 = predict_functional.predict_functional(result3, "year", ci_method="simultaneous", values={"logpop": 11})
+pred3, cb3, fvals3 = predict_functional.predict_functional(result3, "year", ci_method="simultaneous", values={"logpop": 12})
+
+pdf = PdfPages("year_built.pdf")
+
+plt.clf()
+plt.plot(np.log(qa.fit), np.log(qa.obs), 'o')
+x = np.linspace(-0.5, 4, 10)
+plt.plot(x, np.log(np.exp(x) + 0.5*np.exp(2*x)), '-')
+plt.xlabel("log mean", size=15)
+plt.ylabel("log variance", size=15)
+plt.grid(True)
+pdf.savefig()
+
+plt.clf()
+plt.plot(np.log(qa.fit), np.log(qa.obs - qa.fit), 'o')
+x = np.linspace(-0.5, 4, 10)
+plt.plot(x, np.log(0.5) + 2*x, '-')
+plt.xlabel("log mean", size=15)
+plt.ylabel("log (variance - mean)", size=15)
+plt.grid(True)
+pdf.savefig()
+
+plt.clf()
+plt.plot(result2.cov_struct.dep_params)
+plt.grid(True)
+plt.gca().set_xticks(range(18))
+plt.xlim(0, 18)
+plt.xlabel("Lag (years)", size=15)
+plt.ylabel("Autocorrelation", size=15)
+pdf.savefig()
+
+for k in range(2):
+    plt.clf()
+    plt.axes([0.15, 0.1, 0.72, 0.86])
+    if k == 0:
+        plt.plot(fvals1, pred1, '-', label="10")
+        plt.plot(fvals2, pred2, '-', label="11")
+        plt.plot(fvals3, pred3, '-', label="12")
+        plt.fill_between(fvals1, cb1[:, 0], cb1[:, 1], color='grey')
+        plt.fill_between(fvals2, cb2[:, 0], cb2[:, 1], color='grey')
+        plt.fill_between(fvals3, cb3[:, 0], cb3[:, 1], color='grey')
+        plt.ylabel("Housing starts (log scale)", size=15)
+    else:
+        plt.plot(fvals1, 100*np.exp(pred1), '-', label="10")
+        plt.plot(fvals2, 100*np.exp(pred2), '-', label="11")
+        plt.plot(fvals3, 100*np.exp(pred3), '-', label="12")
+        plt.fill_between(fvals1, 100*np.exp(cb1[:, 0]), 100*np.exp(cb1[:, 1]), color='grey')
+        plt.fill_between(fvals2, 100*np.exp(cb2[:, 0]), 100*np.exp(cb2[:, 1]), color='grey')
+        plt.fill_between(fvals3, 100*np.exp(cb3[:, 0]), 100*np.exp(cb3[:, 1]), color='grey')
+        plt.ylabel("Housing starts (US projection)", size=15)
+    ha, lb = plt.gca().get_legend_handles_labels()
+    leg = plt.figlegend(ha, lb, "center right")
+    leg.set_title("log pop")
+    leg.draw_frame(False)
+    plt.xlabel("Year", size=15)
+    pdf.savefig()
+
+nb["resid"] = result1.resid_pearson
+xt = nb.pivot("fips", "year", "resid")
+
+xm = np.asarray(pd.isnull(xt)).astype(np.int)
+x0 = np.asarray(xt.fillna(value=xt.mean().mean()))
+
+d = 3
+for k in range(5):
+    u,s,vt = np.linalg.svd(x0, 0)
+    u = u[:, 0:d]
+    s = s[0:d]
+    vt = vt[0:d, :]
+    xp = np.dot(u * s, vt)
+    x0 = xp*xm + x0*(1-xm)
+
+u,s,vt = np.linalg.svd(x0, 0)    
+
+plt.clf()
+plt.title("Singular values of completed residuals")
+plt.plot(1960 + np.arange(len(s)), s)
+plt.grid(True)
+plt.xlabel("Component", size=15)
+plt.ylabel("Singular value", size=15)
+pdf.savefig()
+
+plt.clf()
+plt.title("Singular vectors of completed residuals")
+plt.plot(vt[0,:], label="1")
+plt.plot(vt[1,:], label="2")
+plt.grid(True)
+plt.xlabel("Year", size=15)
+plt.ylabel("Singular vector loading", size=15)
+ha,lb = plt.gca().get_legend_handles_labels()
+leg = plt.figlegend(ha, lb, "center right")
+leg.draw_frame(False)
+pdf.savefig()
+
+plt.clf()
+plt.hist(u[:,0] * s[0])
+plt.title("Component 1")
+plt.xlabel("FIPS region coefficient", size=15)
+plt.ylabel("Frequency", size=15)
+pdf.savefig()
+
+plt.clf()
+plt.hist(u[:,1] * s[1])
+plt.title("Component 2")
+plt.xlabel("FIPS region coefficient", size=15)
+plt.ylabel("Frequency", size=15)
+pdf.savefig()
+
+pdf.close()
