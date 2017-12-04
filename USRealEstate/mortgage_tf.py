147d8fdab909320cf70273f1376ef1749c03d7c9
Kerby Shedden <kshedden@umich.edu>
2017-12-04 08:38:37 -0500

    update

--- USRealEstate/mortgage_tf.py
+++ USRealEstate/mortgage_tf.py
@@ -66,28 +66,49 @@ def gen_predict_fn(z):
 features = [tf.contrib.layers.real_valued_column("sale_amt", dimension=1),
             tf.contrib.layers.real_valued_column("year", dimension=1)]
 
-estimator = tf.contrib.learn.DNNRegressor(feature_columns=features, hidden_units=[512, 256])
-estimator.fit(input_fn=input_fn, steps=100)
 
 pdf = PdfPages("mortgage_tf.pdf")
 
-plt.clf()
-plt.axes([0.12, 0.1, 0.76, 0.8])
+for units in ([1,], [2,], [4,], [8,], [16,], [32,], [64,], [128,], [256,], [512,], 
+              [512, 256]): #, [512, 256, 128], [512, 256, 128, 64], [512, 256, 128, 64, 32]):
+    for steps in 50,100,500:
+        for jo in 0,1:
+            for lr in 0.01,0.1:
 
-for p in 16,17,18,19:
-    z = []
-    for x in estimator.predict(input_fn=gen_predict_fn(p)):
-        z.append(x)
-    z = np.asarray(z)
-    z = z * (mort_amt.max() - mort_amt.min()) + mort_amt.min()
-    plt.plot(np.arange(1980, 2016), z, '-', label=str(p))
+                if jo == 0:
+                    optname = "GD"
+                    opt = tf.train.GradientDescentOptimizer(learning_rate=lr)
+                elif jo == 1:
+                    optname = "Adam"
+                    opt = tf.train.AdamOptimizer(learning_rate=lr)
 
-ha,lb = plt.gca().get_legend_handles_labels()
-leg = plt.figlegend(ha, lb, "center right")
-leg.draw_frame(False)
-plt.grid(True)
-plt.xlabel("Year", size=15)
-plt.ylabel("Mortgage (log2)", size=15)
-pdf.savefig()
+                estimator = tf.contrib.learn.DNNRegressor(feature_columns=features, hidden_units=units,
+                                                          optimizer=opt)
+                estimator.fit(input_fn=input_fn, steps=steps)
+
+                # Get final training loss
+                ev = estimator.evaluate(input_fn=input_fn, steps=1)
+                loss = ev['loss']
+
+                plt.clf()
+                plt.axes([0.12, 0.1, 0.76, 0.8])
+
+                for p in 16,17,18,19:
+                    z = []
+                    for x in estimator.predict(input_fn=gen_predict_fn(p)):
+                        z.append(x)
+                    z = np.asarray(z)
+                    z = z * (mort_amt.max() - mort_amt.min()) + mort_amt.min()
+                    plt.plot(np.arange(1980, 2016), z, '-', label=str(p))
+
+                ha,lb = plt.gca().get_legend_handles_labels()
+                leg = plt.figlegend(ha, lb, "center right")
+                leg.draw_frame(False)
+                plt.grid(True)
+                plt.xlabel("Year", size=15)
+                plt.ylabel("Mortgage (log2)", size=15)
+                plt.title("units=%s, steps=%s, rate=%.2f, %s optimizer, loss=%.4f" % 
+                          (str(units), str(steps), lr, optname, 100*loss))
+                pdf.savefig()
 
 pdf.close()
